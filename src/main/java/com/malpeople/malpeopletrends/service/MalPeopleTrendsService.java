8f4ffac3691b2ca261317e2d876a2b4c24d8e0fd
Martin <martinpayne73@gmail.com>
2022-12-05 01:38:53 -0500

    fix mal link and rank

--- src/main/java/com/malpeople/malpeopletrends/service/MalPeopleTrendsService.java
+++ src/main/java/com/malpeople/malpeopletrends/service/MalPeopleTrendsService.java
@@ -5,6 +5,7 @@ import com.malpeople.malpeopletrends.model.Person;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,10 +17,11 @@ public class MalPeopleTrendsService {
     }
 
     public PeoplePage getPeopleTrendsOneDay(int offset, String tableName) {
+        var rank = new AtomicInteger(1);
         var filteredList = jdbcTemplate.query(
                 "select * from " + tableName,
                 (rs, rowNum) -> new Person(
-                    Integer.toString(rowNum + 1),
+                    "",
                     rs.getString("english_name"),
                     rs.getString("japanese_name"),
                     rs.getString("mal_link"),
@@ -30,7 +32,18 @@ public class MalPeopleTrendsService {
                 )
             ).stream()
             .filter(person -> Integer.parseInt(person.favoritesChange()) != 0)
+                .map(p -> new Person(
+                        Integer.toString(rank.getAndIncrement()),
+                        p.englishName(),
+                        p.japaneseName(),
+                        p.malLink(),
+                        p.imageLink(),
+                        p.oldFavoriteCount(),
+                        p.newFavoriteCount(),
+                        p.favoritesChange()
+                ))
             .collect(Collectors.toList());
+
         var totalRecords = filteredList.size();
         return new PeoplePage(filteredList.subList(offset, offset + 50), totalRecords);
     }
